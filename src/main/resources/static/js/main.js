65653227d5a9e1fcaf08d6d1d86ec9c2bccf465d
pavel pimenov <pimenov_pa@nlmk.com>
2022-04-26 16:12:47 +0300

    * translate html

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -76,10 +76,10 @@ function onMessageReceived(payload) {
 
     if(message.type === 'JOIN') {
         messageElement.classList.add('event-message');
-        message.content = message.sender + ' joined!';
+        message.content = message.sender + ' подключился!';
     } else if (message.type === 'LEAVE') {
         messageElement.classList.add('event-message');
-        message.content = message.sender + ' left!';
+        message.content = message.sender + ' отключился!';
     } else {
         messageElement.classList.add('chat-message');
 
